a18c11a8f75d192fa2b258730078c4c16bd87450
kylelambert001 <kylelambert001@gmail.com>
2021-04-10 16:13:41 +0930

    Logic to disable buttons if currently loading.

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,13 +1,43 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./Pagination.scss";
 
-function Pagination({ nextPageUrl, previousPageUrl, handleNextPage, handlePreviousPage }) {
+Pagination.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  nextPageUrl: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  previousPageUrl: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  handleNextPage: PropTypes.func.isRequired,
+  handlePreviousPage: PropTypes.func.isRequired,
+};
+
+function Pagination({ loading, nextPageUrl, previousPageUrl, handleNextPage, handlePreviousPage }) {
+  const [disableNext, setDisableNext] = React.useState(false);
+  const [disablePrevious, setDisablePrevious] = React.useState(false);
+
+  const handleNextClick = () => {
+    setDisableNext(true);
+    handleNextPage();
+    setTimeout(() => setDisableNext(false), 500);
+  };
+
+  const handlePreviousClick = () => {
+    setDisablePrevious(true);
+    handlePreviousPage();
+    setTimeout(() => setDisablePrevious(false), 500);
+  };
+
   return (
     <div className="Pagination">
-      <button disabled={!previousPageUrl} onClick={handlePreviousPage} className="Pagination__btn">
+      <button
+        disabled={!previousPageUrl || disablePrevious || loading}
+        onClick={handlePreviousClick}
+        className="Pagination__btn">
         Previous
       </button>
-      <button disabled={!nextPageUrl} onClick={handleNextPage} className="Pagination__btn">
+      <button
+        disabled={!nextPageUrl || disableNext || loading}
+        onClick={handleNextClick}
+        className="Pagination__btn">
         Next
       </button>
     </div>
